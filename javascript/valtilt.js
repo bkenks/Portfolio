caa78bfb16be401ceae3e670bcb6e032d10d0e9a
bkenks <briankenkel.t@gmail.com>
2024-01-22 21:56:51 -0500

    draft complete

--- javascript/valtilt.js
+++ javascript/valtilt.js
@@ -1,8 +1,7 @@
 let tilt = document.querySelectorAll(".rounded");
 VanillaTilt.init(tilt, {
-  max: 3,
+  max: 8,
   speed: 500,
   scale: 1.05,
-  glare: true,
-  "max-glare": 0.3,
+  glare: false,
 });
